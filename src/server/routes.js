966246026e2a2e51e966e83cc6635958bbbc70d0
Ganar KF <gnrkfz@gmail.com>
2024-03-31 22:29:56 +0700

    Commit 2

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,18 +1,6 @@
 const postPredictHandler = require('../server/handler');
 
-const rootHandler = (request, h) => {
-    return h.response({
-        status: 'success',
-        message: 'Welcome to the root endpoint!'
-    }).code(200);
-};
-
 const routes = [
-    {
-        path: '/',
-        method: 'GET',
-        handler: rootHandler
-    },
     {
         path: '/predict',
         method: 'POST',
